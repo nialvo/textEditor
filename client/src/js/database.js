4a986c098c2c3278723a5964c6554bbf54028655
Ignaz Protagonist <pilibili@protonmail.com>
2022-05-04 05:16:04 -0500

    added missing elements, sometimes doesn't work locally, not sure why

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,31 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+
+  //open jate database
+  const jDb = await openDB('jate', 1);
+  //open a writeable transaction
+  const tx = jDb.transaction('jate', 'readwrite');
+  //add the content to the unique store property of the transaction, 
+  //the new await automatically closes the transaction
+  const res = await tx.store.put({ id: 1, value: content });
+};
+
+
+export const getDb = async () => {
+
+  //open jate database
+  const jDb = await openDB('jate', 1);
+  //open a read-only transaction
+  const tx = jDb.transaction('jate', 'readonly');
+  //get the content from the unique store property of the transaction, 
+  //the new await automatically closes the transaction
+  const res = await tx.store.get(1);
+  //return the obtained content
+  return res?.value;
+
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,64 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+
+      //for html generation
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
+
+      //for precache
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      //for manifest.json generation
+      new WebpackPwaManifest({
+        fingerprints: false,//disable browser fingerprinting
+        //set theme and background colors
+        inject: true,
+        theme_color: '#ffb05c',
+        background_color: '#ff6198',
+        //set name and description
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        //set user path
+        start_url: '/',
+        publicPath: '/',
+        //link icon
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
       
     ],
 
     module: {
       rules: [
+        //add rule for loading css 
+        {
+          test: /\.css$/i,//regex for css file names
+          use: ['style-loader', 'css-loader'],
+        },
+        //add rule for loading js files
+        {
+          test: /\.m?js$/,//regex for js and mjs file names
+          exclude: /node_modules/,//exclude node modules
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
         
       ],
     },
